10a394e9daae755adfaa85c183c90869a781bcef
tanvi200323 <113408281+tanvi200323@users.noreply.github.com>
2023-08-08 09:43:00 +0530

    First Commit

---
+++ com/Strings/StringsComparison.java
@@ -0,0 +1,19 @@
+package com.Strings;
+
+public class StringsComparison {
+    public static void main(String[] args) {
+        //SCP
+        String name1= "tanvi";
+        String name2= "tanvi";
+
+        System.out.println(name1 == name2);
+        System.out.println(name1.equals(name2));
+
+        //Heap Memory
+        String a = new String("tanvi");
+        String b = new String("tanvi");
+
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+    }
+}
