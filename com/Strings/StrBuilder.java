9e706b3002a370b9a802404d3e0ad96fd7fe4622
tanvi200323 <113408281+tanvi200323@users.noreply.github.com>
2023-08-08 20:56:33 +0530

    First Commit

---
+++ com/Strings/StrBuilder.java
@@ -0,0 +1,23 @@
+package com.Strings;
+
+public class StrBuilder {
+    public static void main(String[] args) {
+        //print a-z by string:
+        String series = "";
+        for(int i=0; i<26; i++){
+            char ch = (char)('a' + i);
+            series += ch;
+        }
+        System.out.println(series);
+        //String will create new object everytime and Memory will be wasted giving O(N^2) complexity
+        //So, we need a dataype that creates only 1 object and changes are made in that object only instead of making new objects
+        //So, that datatype is called - STRINGBUILDER
+
+        StringBuilder series2 = new StringBuilder();
+        for(int i=0;i<26;i++){
+            char ch = (char)('a'+i);
+            series2.append(ch); //appending characters in 1 object
+        }
+        System.out.println(series2);
+    }
+}
