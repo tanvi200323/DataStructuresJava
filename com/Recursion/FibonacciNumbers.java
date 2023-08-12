be51bbe8cccefff9c162bf2c8f8e646c79689a35
tanvi200323 <113408281+tanvi200323@users.noreply.github.com>
2023-08-12 23:37:33 +0530

    First Commit

---
+++ com/Recursion/FibonacciNumbers.java
@@ -0,0 +1,20 @@
+package com.Recursion;
+
+public class FibonacciNumbers {
+    public static void main(String[] args) {
+        int n = 4;
+        int ans = fibo(n);
+        System.out.println(ans);
+    }
+    static int fibo(int n){
+        if(n == 0){
+            return 0;
+        }
+        else if(n == 1){
+            return 1;
+        }
+        else{
+            return fibo(n-1) + fibo(n-2); //linear recurrence relation , inefficient
+        }
+    }
+}
