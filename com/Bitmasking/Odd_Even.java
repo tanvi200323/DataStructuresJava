07af3ce1c90c45ad7e1ddba42fdca4583901981a
tanvi200323 <113408281+tanvi200323@users.noreply.github.com>
2023-08-15 12:04:38 +0530

    First Commit

---
+++ com/Bitmasking/Odd_Even.java
@@ -0,0 +1,14 @@
+package com.Bitmasking;
+
+public class Odd_Even {
+    public static void main(String[] args) {
+        int n = 32;
+        //if(n%2 == 0)   --> slow method
+        if((n&1) == 0){   //even number ends with 0
+            System.out.println("Even");
+        }
+        else {            //odd number ends with 1
+            System.out.println("Odd");
+        }
+    }
+}
--- com/Recursion/_2FibonacciNumbers.java
+++ com/Recursion/_2FibonacciNumbers.java
@@ -5,6 +5,10 @@ public class _2FibonacciNumbers {
         int n = 4;
         int ans = fibo(n);
         System.out.println(ans);
+        System.out.println(fiboformula(50));
+    }
+    static int fiboformula(int n){
+        return (int)(Math.pow((( 1 + Math.sqrt(5))/ 2 ), n) /Math.sqrt(5));
     }
     static int fibo(int n){
         if(n == 0){
