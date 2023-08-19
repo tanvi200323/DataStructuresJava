a504f900e88cee632747bafdd37340f22ba4f392
tanvi200323 <113408281+tanvi200323@users.noreply.github.com>
2023-08-20 01:40:13 +0530

    First Commit

---
+++ com/Recursion/_13LinearSearchRec.java
@@ -0,0 +1,67 @@
+package com.Recursion;
+
+import java.util.ArrayList;
+
+public class _13LinearSearchRec {
+    public static void main(String[] args) {
+        int[] arr = {3,2,1,18,18,9};
+        int ans = findIndex(arr, 0, 9);
+        System.out.println(ans);
+        findallindex1(arr,0,18);
+        System.out.println(list);
+        int[] arr2 = {1,2,3,6,4,4,5};
+
+        ArrayList<Integer> listt = new ArrayList<>();
+        ArrayList<Integer> ans2 = findallindex2(arr2,0,4,listt);
+//        ArrayList<Integer> ans2 = findallindex2(arr2,0,4,new ArrayList<>());
+        System.out.println(ans2);
+        System.out.println(listt);
+        System.out.println(findallindex3(arr2,0,4));
+    }
+    static int findIndex(int[] arr, int index, int target){
+        if(index == arr.length){
+            return -1;
+        }
+        if(arr[index] == target){
+            return index;
+        }
+        return findIndex(arr,index+1,target);
+    }
+    //if we want to print a arraylist:
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void findallindex1(int[] arr, int index, int target){
+        if(index == arr.length){
+            return;
+        }
+        if(arr[index] == target){
+            list.add(index);
+        }
+        findallindex1(arr, index+1, target);
+    }
+
+    //if we want to return the arraylist:
+    static ArrayList<Integer> findallindex2(int[] arr, int index, int target, ArrayList<Integer> list2){
+        if(index == arr.length){
+            return list2;
+        }
+        if(arr[index] == target){
+            list2.add(index);
+        }
+        return findallindex2(arr, index+1, target, list2);
+    }
+
+    //returning arraylist without passing the arguement
+    static ArrayList<Integer> findallindex3(int[] arr,int index, int target){
+        ArrayList<Integer> ls = new ArrayList<>();
+        if(index == arr.length){
+            return ls;
+        }
+        //stores answers for that function call only
+        if(arr[index]==target){
+            ls.add(index);
+        }
+        ArrayList<Integer> addFromBelowCalls = findallindex3(arr, index+1, target);
+        ls.addAll(addFromBelowCalls);
+        return ls;
+    }
+}
---
+++ com/Recursion/_14RotatedBinarySearch.java
@@ -0,0 +1,34 @@
+package com.Recursion;
+
+public class _14RotatedBinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {60,70,10,20,30,40,50};
+        System.out.println(rotatedbs(arr,40,0,arr.length-1));
+        System.out.println(rotatedbs(arr,11,0,arr.length-1));
+    }
+    static int rotatedbs(int[] arr, int target, int start, int end){
+        if(start>end){
+            return -1;
+        }
+        int mid = start+(end-start)/2;
+        if(arr[mid] == target){
+            return mid;
+        }
+        if(arr[start]<=arr[mid]){
+            if(target>=arr[start] && target<arr[mid]){
+                return rotatedbs(arr,target,start,mid-1);
+            }
+            else{
+                return rotatedbs(arr,target,mid+1,end);
+            }
+        }
+        else{
+            if(target>arr[mid] && target<=arr[end]){
+                return rotatedbs(arr, target, mid+1, end);
+            }
+            else{
+                return rotatedbs(arr,target,start,mid-1);
+            }
+        }
+    }
+}
